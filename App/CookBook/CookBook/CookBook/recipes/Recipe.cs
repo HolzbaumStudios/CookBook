7bf692dda8c4a10ba1e45db4a34b7d410a27075e
Felipe Blanco <fblanco@totemo.net>
2016-12-19 14:18:56 +0100

    Started setting up backend

---
+++ App/CookBook/CookBook/CookBook/recipes/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.recipes
+{
+    class Recipe
+    {
+        public String name { get; set; }
+        public String description { get; set; }
+        public String creator { get; set; }
+        public String imagePath { get; set; }
+
+        public List<Step> steps { get; set; }
+    }
+}   
