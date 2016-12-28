a4d4a116c41f3c5ee2952e4c55947cfe5ca3d600
fbHolzbaum <blancofel22@gmail.com>
2016-12-28 10:57:19 +0100

    Updated SQL Resources
    Added sql strings for images

--- app/CookBook/CookBook/DBResources/SqlResources.cs
+++ app/CookBook/CookBook/DBResources/SqlResources.cs
@@ -13,5 +13,8 @@ namespace CookBook.resources
         public static readonly String RECIPES_UPDATE = "UPDATE RECIPES "
                                                      + "SET recipes_name=@name, recipes_description=@desc, creator=@creator, fk_image=@imageId) "
                                                      + "WHERE id_recipes=@id";
+        public static readonly String IMAGES_INSERT_RETURN = "INSERT INTO IMAGES (storage_path) VALUES (@storagePath) "
+                                                     + "SELECT LAST_INSERT_ID()";
+        public static readonly String IMAGES_UPDATE = "UPDATE IMAGES SET storage_path=@storagePath WHERE id_images=@id";
     }
 }
