7d88853dbc878cbd1aff9e40fe1294209ec6cdb0
fbHolzbaum <blancofel22@gmail.com>
2017-01-25 17:02:57 +0100

    Added Admin Form Funcitonality

--- App/CookBook/AdminConsole/AdminForm.Designer.cs
+++ App/CookBook/AdminConsole/AdminForm.Designer.cs
@@ -34,12 +34,13 @@
             this.Lable_Creator = new System.Windows.Forms.Label();
             this.TextBox_Creator = new System.Windows.Forms.TextBox();
             this.TextBox_Description = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DataGrid_Steps = new System.Windows.Forms.DataGridView();
             this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ingredients = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Timer = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TimeUnit = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Button_Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid_Steps)).BeginInit();
             this.SuspendLayout();
             // 
             // LabelRecipeName
@@ -50,7 +51,6 @@
             this.LabelRecipeName.Size = new System.Drawing.Size(35, 13);
             this.LabelRecipeName.TabIndex = 0;
             this.LabelRecipeName.Text = "Name";
-            this.LabelRecipeName.Click += new System.EventHandler(this.label1_Click);
             // 
             // TextBox_RecipeName
             // 
@@ -58,7 +58,6 @@
             this.TextBox_RecipeName.Name = "TextBox_RecipeName";
             this.TextBox_RecipeName.Size = new System.Drawing.Size(136, 20);
             this.TextBox_RecipeName.TabIndex = 1;
-            this.TextBox_RecipeName.TextChanged += new System.EventHandler(this.TextBox_RecipeName_TextChanged);
             // 
             // Label_Description
             // 
@@ -77,7 +76,6 @@
             this.Lable_Creator.Size = new System.Drawing.Size(41, 13);
             this.Lable_Creator.TabIndex = 0;
             this.Lable_Creator.Text = "Creator";
-            this.Lable_Creator.Click += new System.EventHandler(this.label1_Click);
             // 
             // TextBox_Creator
             // 
@@ -93,21 +91,19 @@
             this.TextBox_Description.Name = "TextBox_Description";
             this.TextBox_Description.Size = new System.Drawing.Size(546, 86);
             this.TextBox_Description.TabIndex = 1;
-            this.TextBox_Description.TextChanged += new System.EventHandler(this.TextBox_RecipeName_TextChanged);
             // 
-            // dataGridView1
+            // DataGrid_Steps
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DataGrid_Steps.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGrid_Steps.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Description,
             this.Ingredients,
             this.Timer,
             this.TimeUnit});
-            this.dataGridView1.Location = new System.Drawing.Point(16, 137);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(745, 167);
-            this.dataGridView1.TabIndex = 3;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.DataGrid_Steps.Location = new System.Drawing.Point(16, 137);
+            this.DataGrid_Steps.Name = "DataGrid_Steps";
+            this.DataGrid_Steps.Size = new System.Drawing.Size(745, 167);
+            this.DataGrid_Steps.TabIndex = 3;
             // 
             // Description
             // 
@@ -136,12 +132,23 @@
             "Hours"});
             this.TimeUnit.Name = "TimeUnit";
             // 
+            // Button_Save
+            // 
+            this.Button_Save.Location = new System.Drawing.Point(634, 313);
+            this.Button_Save.Name = "Button_Save";
+            this.Button_Save.Size = new System.Drawing.Size(127, 23);
+            this.Button_Save.TabIndex = 4;
+            this.Button_Save.Text = "Save Recipe";
+            this.Button_Save.UseVisualStyleBackColor = true;
+            this.Button_Save.Click += new System.EventHandler(this.Button_Save_Click);
+            // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(774, 316);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(774, 348);
+            this.Controls.Add(this.Button_Save);
+            this.Controls.Add(this.DataGrid_Steps);
             this.Controls.Add(this.Label_Description);
             this.Controls.Add(this.TextBox_Creator);
             this.Controls.Add(this.Lable_Creator);
@@ -150,7 +157,7 @@
             this.Controls.Add(this.LabelRecipeName);
             this.Name = "AdminForm";
             this.Text = "Create Recipes";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid_Steps)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,11 +171,12 @@
         private System.Windows.Forms.Label Lable_Creator;
         private System.Windows.Forms.TextBox TextBox_Creator;
         private System.Windows.Forms.TextBox TextBox_Description;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DataGrid_Steps;
         private System.Windows.Forms.DataGridViewTextBoxColumn Description;
         private System.Windows.Forms.DataGridViewTextBoxColumn Ingredients;
         private System.Windows.Forms.DataGridViewTextBoxColumn Timer;
         private System.Windows.Forms.DataGridViewComboBoxColumn TimeUnit;
+        private System.Windows.Forms.Button Button_Save;
     }
 }
 
--- App/CookBook/AdminConsole/AdminForm.cs
+++ App/CookBook/AdminConsole/AdminForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CookBook.recipes;
+using CookBook.Recipes;
 
 namespace AdminConsole
 {
@@ -17,19 +19,60 @@ namespace AdminConsole
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Button_Save_Click(object sender, EventArgs e)
         {
+            var recipe = new Recipe();
+            recipe.Name = TextBox_RecipeName.Text;
+            recipe.Creator = TextBox_Creator.Text;
+            recipe.Description = TextBox_Description.Text;
+            var stepDataSet = DataGrid_Steps;
+            foreach(DataRow row in stepDataSet.Rows)
+            {
+                String stepDescription = (String)row.ItemArray.GetValue(0);
+                String ingredientString = (String)row.ItemArray.GetValue(1);
+                int timer = Int32.Parse((String)row.ItemArray.GetValue(2));
+                String timeUnit = row.ItemArray.GetValue(3).ToString();
 
+                Step newStep = new Step();
+                newStep.Description = stepDescription;
+                newStep.SetTimer(timer, GetTimeUnit(timeUnit));
+                ProcessIngredient(ref newStep, ingredientString);
+                recipe.AddStep(newStep);
+            }
+            var rm = new RecipeManager();
+            rm.StoreRecipe(recipe);
         }
 
-        private void TextBox_RecipeName_TextChanged(object sender, EventArgs e)
+        private TimeUnits GetTimeUnit(String timeUnitString)
         {
-
+            TimeUnits selectedTimeUnit;
+            switch(timeUnitString.ToLower())
+            {
+                case "seconds": selectedTimeUnit = TimeUnits.Seconds;
+                    break;
+                case "minutes": selectedTimeUnit = TimeUnits.Minutes;
+                    break;
+                case "hours": selectedTimeUnit = TimeUnits.Hours;
+                    break;
+                default: throw new Exception();
+            }
+            return selectedTimeUnit;
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void ProcessIngredient(ref Step step, String ingredientString)
         {
-
+            String[] ingredients = ingredientString.Split(';');
+            foreach (String ingredient in ingredients)
+            {
+                var newIngredient = new Ingredient();
+                String[] splitIngredient = ingredient.Split(',');
+                if (splitIngredient != null && splitIngredient.Length == 2)
+                {
+                    newIngredient.Name = splitIngredient[0];
+                    newIngredient.Unit = splitIngredient[1];
+                }
+                step.AddIngredient(newIngredient);
+            }
         }
     }
 }
